7b140bd09dd6cf8eb7d88345158a6a87088811c9
BryceKeel <Bkeel24@basecampcodingacademy.org>
2023-11-01 08:43:59 -0500

    hello django

---
+++ BCCA Projects/Unit 5/Module 1/projects/app/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http.request import HttpRequest
+from django.http.response import HttpResponse
+# Create your views here.
+def hello_views(request: HttpRequest) -> HttpResponse:
+    return HttpResponse("Hello World")
